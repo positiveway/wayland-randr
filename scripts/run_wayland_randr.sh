6df22ed9298cec04729ca5919c2f2b7c354eeeff
Andrew <PositiveWay05@gmail.com>
2024-08-03 19:05:16 +0500

    changed default brightness

--- scripts/run_wayland_randr.sh
+++ scripts/run_wayland_randr.sh
@@ -4,5 +4,5 @@ set -e
 
 AppDestDir="$HOME/Documents/wayland-randr"
 
-$AppDestDir/wayland-randr adjust --brightness 0.3 eDP-1
+$AppDestDir/wayland-randr adjust --brightness 0.25 eDP-1
 # ./target/release/wayland-randr adjust --brightness 0.3 eDP-1
