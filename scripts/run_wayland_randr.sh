85d72d73edf3635885d039bdcc9b4ca50b8a0cc1
Andrew <PositiveWay05@gmail.com>
2024-10-01 16:39:57 +0500

    data filepath

--- scripts/run_wayland_randr.sh
+++ scripts/run_wayland_randr.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-DataDir="/mnt/data"
-DocsDir="$DataDir/Documents"
-ScriptsDir="$DocsDir/Scripts"
-
-"$ScriptsDir/wayland-randr" adjust --brightness 0.25 eDP-1
+/mnt/data/Documents/Scripts/wayland-randr adjust --brightness 0.25 eDP-1
 # ./target/release/wayland-randr adjust --brightness 0.3 eDP-1
