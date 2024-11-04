af164787dc205dc2f725c9b31b023fcd4de25f3a
Andrew <PositiveWay05@gmail.com>
2024-11-04 17:43:32 +0500

    egpu

--- scripts/run_wayland_randr.sh
+++ scripts/run_wayland_randr.sh
@@ -7,8 +7,10 @@ eGPUDisplay="eDP-1-1"
 
 Brightness="0.25"
 
+# ./target/release/wayland-randr adjust --brightness 0.3 eDP-1
 
 /mnt/data/Documents/Scripts/wayland-randr adjust --brightness "$Brightness" "$BuiltinDisplay"
 /mnt/data/Documents/Scripts/wayland-randr adjust --brightness "$Brightness" "$eGPUDisplay"
 
-# ./target/release/wayland-randr adjust --brightness 0.3 eDP-1
+#confirm settings
+#xrandr --current --verbose
