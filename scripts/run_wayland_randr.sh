92d056c15aa94c9fab4a5f131aa73393fc90f9e3
Andrew <PositiveWay05@gmail.com>
2025-05-30 05:27:25 +0400

    set 0.35 in run

--- scripts/run_wayland_randr.sh
+++ scripts/run_wayland_randr.sh
@@ -6,6 +6,7 @@ eGPUDisplay="eDP-1-1"
 Brightness="0.25"
 
 # ./target/release/wayland-randr adjust --brightness 0.3 eDP-1
+# /mnt/data/Documents/Scripts/wayland-randr adjust --brightness 0.35 eDP-1
 
 /mnt/data/Documents/Scripts/wayland-randr adjust --brightness "$Brightness" "$BuiltinDisplay"
 /mnt/data/Documents/Scripts/wayland-randr adjust --brightness "$Brightness" "$eGPUDisplay"
